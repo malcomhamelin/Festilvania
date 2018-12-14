f945bdb13ee8c6529d7c987a03ea7eedc4ebc372
kyuuruu <45665066+kyuuruu@users.noreply.github.com>
2018-12-14 15:37:33 +0100

    Add files via upload

--- Festilvania/modules/mod_register/model_register.php
+++ Festilvania/modules/mod_register/model_register.php
@@ -1,13 +1,9 @@
+
 <?php
-
 require_once "connection.php";
-
 Class ModelRegister extends Connection {
-
     public function __construct() {
-
     }
-
     public function is_clean($string) {
        return ! (preg_match("/[^a-z\d_-] /i", $string));
        
@@ -19,31 +15,21 @@ Class ModelRegister extends Connection {
         //variable
         if (isset($_POST['pseudo'])) {
             $pseudo = $_POST['pseudo']; } 
-
         if (isset($_POST['email'])) {
             $email = $_POST['email']; } 
-
         if (isset($_POST['password'])) {
             $password = $_POST['password']; } 
-
         if (isset($_POST['password2'])) {
             $password2 = $_POST['password2']; } 
-
         if (isset($_POST['sexe'])) { 
             $sexe = $_POST['sexe']; } 
-
         if (isset($_POST['anniversaire'])) { 
             $anniversaire = $_POST['anniversaire']; } 
-
         if (isset($_POST['avatar'])) { 
             $avat = $_POST['avatar']; } 
-
         if(isset($_POST['pseudo'])&&isset($_POST['email'])&&isset($_POST['password'])&&isset($_POST['sexe'])&&isset($_POST['anniversaire'])&&isset($_POST['avatar'])){
-
             if(self::is_clean($pseudo)&&self::is_email($email)&&self::is_clean($password)&&self::is_clean($password2)){
-
                 if($password==$password2){
-
                     $verifEmailunique =self::$bdd->prepare("SELECT count(*) from membre where membre.mail=:mail");
                     $verifEmailunique->bindParam(':mail', $email);
                     $verifEmailunique->execute();
@@ -51,20 +37,25 @@ Class ModelRegister extends Connection {
                     var_dump($tab);
                     
                     if(0==$tab[0]){
-
-
-                        $req=self::$bdd->prepare("INSERT INTO membre VALUES(default,:Pseudo,:Password,:Mail,NOW(),:Avatar,1,:Sexe,:Anniv)");
-
-                        $req->bindParam(':Pseudo', $pseudo);
-                        $req->bindParam(':Password', $password);
-                        $req->bindParam(':Mail', $email);
-                        $req->bindParam(':Avatar',$avat);
-                        $req->bindParam(':Sexe', $sexe);
-                        $req->bindParam(':Anniv', $anniversaire);
-
-                        $req -> execute();
+                        $verifPseudolunique =self::$bdd->prepare("SELECT count(*) from membre where membre.mail=:mail");
+                        $verifPseudolunique->bindParam(':mail', $email);
+                        $verifPseudolunique->execute();
+                        $tab=$verifPseudolunique->fetch();
+                    
+                        if(0==$tab[0]){ 
+                            $req=self::$bdd->prepare("INSERT INTO membre VALUES(default,:Pseudo,:Password,:Mail,NOW(),:Avatar,1,:Sexe,:Anniv)");
+                            $req->bindParam(':Pseudo', $pseudo);
+                            $req->bindParam(':Password', $password);
+                            $req->bindParam(':Mail', $email);
+                            $req->bindParam(':Avatar',$avat);
+                            $req->bindParam(':Sexe', $sexe);
+                            $req->bindParam(':Anniv', $anniversaire);
+                            $req -> execute();
+                        }
+                        else {
+                            echo'pseudo pris';
+                        }
                     }
-
                     else {
                         echo'email pris';
                     }
@@ -76,12 +67,9 @@ Class ModelRegister extends Connection {
             else{
                 echo"inscription impossible,caractères spéciaux interdit"; 
             }
-
         }
-
     }
     
-
 }
-
 ?>
+
