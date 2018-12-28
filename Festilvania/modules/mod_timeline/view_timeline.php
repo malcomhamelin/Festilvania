d26a69e4334b7de63a4158816b580239d698e1cf
DINHJonathan <45485650+DINHJonathan@users.noreply.github.com>
2018-12-28 19:39:08 +0100

    Changement de variable idEvent en idEvenement

--- Festilvania/modules/mod_timeline/view_timeline.php
+++ Festilvania/modules/mod_timeline/view_timeline.php
@@ -20,9 +20,9 @@ Class ViewTimeline extends ViewGeneric {
                 echo    '<div class="container annonce shadow-sm mb-5">
                             <div class="row">
                                 <div class="col-2 col-md-1 col-lg-1 votes ml-5 my-auto">
-                                    <a href="index.php?mod=timeline&action=upvote&idEvent=' . $key['idEvenement'] . '"><img src="img/nexttg.png" alt="upvote" class="votes-img"></a>
+                                    <a href="index.php?mod=timeline&action=upvote&idEvenement=' . $key['idEvenement'] . '"><img src="img/nexttg.png" alt="upvote" class="votes-img"></a>
                                     <div class="btn font-weight-bold"><span>' . $nbVotes . '</span></div>
-                                    <a href="index.php?mod=timeline&action=downvote&idEvent=' . $key['idEvenement'] . '"><img src="img/nextbg.png" alt="downvote" class="votes-img"></a>
+                                    <a href="index.php?mod=timeline&action=downvote&idEvenement=' . $key['idEvenement'] . '"><img src="img/nextbg.png" alt="downvote" class="votes-img"></a>
                                 </div>
                     
                                 <div class="col-7 col-md-4 col-lg-3 annonce-col">
@@ -32,7 +32,7 @@ Class ViewTimeline extends ViewGeneric {
                                 <div class="col-12 col-md-6 annonce-corps">
                                     <div class="row mx-auto">
                                         <div class="col-12 col-sm-12 col-md-12">
-                                            <span class="my-auto annonce-corps-titre"><a href="index.php?mod=post&idEvent=' . $key['idEvenement'] . '">' . $key['titreEvenement'] . '</a></span>
+                                            <span class="my-auto annonce-corps-titre"><a href="index.php?mod=post&idEvenement=' . $key['idEvenement'] . '">' . $key['titreEvenement'] . '</a></span>
                                             <span class="my-auto font-weight-bold" id="annonce-corps-infos">01/01/2018 - 02/03/2018 à Paris</span>
                                         </div>
                                     </div>
@@ -41,7 +41,7 @@ Class ViewTimeline extends ViewGeneric {
                                             
                                             $this->getScheduleButton($userInfos, $key['idEvenement']);
 
-                echo                        '<a href="index.php?mod=post&idEvent=' . $key['idEvenement'] . '"><div class="btn btn-warning annonce-corps-btn ml-3">Voir l\'évènement</div></a>
+                echo                        '<a href="index.php?mod=post&idEvenement=' . $key['idEvenement'] . '"><div class="btn btn-warning annonce-corps-btn ml-3">Voir l\'évènement</div></a>
                                         </div>
                                     </div>
                                 </div>
@@ -68,10 +68,10 @@ Class ViewTimeline extends ViewGeneric {
         }
 
         if ($isPresentSchedule)  {
-            echo '<a href="index.php?mod=timeline&action=delschedule&option=' . $_SESSION['option'] . '&idEvent=' . $idEvenement . '"><div class="btn btn-warning annonce-corps-btn mx-auto" title="Retirer de mon agenda"><i class="fas fa-minus"></i></div></a>';
+            echo '<a href="index.php?mod=timeline&action=delschedule&option=' . $_SESSION['option'] . '&idEvenement=' . $idEvenement . '"><div class="btn btn-warning annonce-corps-btn mx-auto" title="Retirer de mon agenda"><i class="fas fa-minus"></i></div></a>';
         }
         else {
-            echo '<a href="index.php?mod=timeline&action=addschedule&option=' . $_SESSION['option'] . '&idEvent=' . $idEvenement . '"><div class="btn btn-warning annonce-corps-btn mx-auto" title="Ajouter à mon agenda"><i class="fas fa-plus"></i></div></a>';
+            echo '<a href="index.php?mod=timeline&action=addschedule&option=' . $_SESSION['option'] . '&idEvenement=' . $idEvenement . '"><div class="btn btn-warning annonce-corps-btn mx-auto" title="Ajouter à mon agenda"><i class="fas fa-plus"></i></div></a>';
         }
     }
 
@@ -83,7 +83,7 @@ Class ViewTimeline extends ViewGeneric {
             echo    '<div class="col-10 col-md-3 mx-auto mb-4 blocs-annexes-annonces">
                         <img src="img/background3.jpg" alt="event1" class="blocs-annexes-photos">
                         <div class="blocs-annexes-annonces-texte ml-2 text-left">
-                            <a href="index.php?mod=post&idEvent=' . $key['idEvenement'] . '">
+                            <a href="index.php?mod=post&idEvenement=' . $key['idEvenement'] . '">
                                 <span class="font-weight-bold blocs-annexes-annonces-texte-titre">' . $key['titreEvenement'] . '</span><br>
                                 <span class="blocs-annexes-annonces-texte-description font-weight-bold">' . $dateBegin->format('d/m/y') . ' - ' . $dateEnd->format('d/m/y') . ' | ' . $key['lieu'] . '</span>
                             </a>
