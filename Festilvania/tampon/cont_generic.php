2cc96669efa34ef5e042ff8cb8e920235f346749
Malcom HAMELIN <39401347+malcomhamelin@users.noreply.github.com>
2019-01-11 12:37:54 +0100

    Ajout du système de token

--- Festilvania/tampon/cont_generic.php
+++ Festilvania/tampon/cont_generic.php
@@ -1,17 +1,45 @@
 <?php
 
-class ContGeneric{
+class ContGeneric {
+
+	private $MAX_SESSION_TIME = '+15 minutes';
 
     private $model;
     private $view;
 
 	public function __construct(){
         $this->model = new ModelGeneric();
-        $this->view = new ViewGeneric();
+		$this->view = new ViewGeneric();
 	}
 
 	public function getDisplay() {
 		return $this->view->getDisplay();
+	}
+
+	public function createToken() {
+		$token = bin2hex(mcrypt_create_iv(32, MCRYPT_DEV_URANDOM));
+
+		$_SESSION['token'] = $token;
+		$_SESSION['tokenCreatedTime'] = new DateTime();
+	}
+
+	public function checkToken() {
+		$expectedMaxTime = $_SESSION['tokenCreatedTime'];
+		$expectedMaxTime->modify($this->MAX_SESSION_TIME);
+
+		$currentTime = new DateTime();
+
+		if (isset($_SESSION['token']) && isset($_POST['token']) && !empty($_SESSION['token']) && !empty($_POST['token']) && 
+			$_SESSION['token'] == $_POST['token'] && $currentTime < $expectedMaxTime) {
+				$_SESSION['token'] = null;
+				$_SESSION['tokenCreatedTime'] = null;
+				return true;
+		}
+
+		$_SESSION['token'] = null;
+		$_SESSION['tokenCreatedTime'] = null;
+		echo '<script type="text/javascript"> errorToken(); </script>';
+		return false;
 	}
 }
 
