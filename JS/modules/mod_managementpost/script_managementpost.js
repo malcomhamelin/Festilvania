8f71d872cdb62539363355f73b1b58730df8805f
Malcom HAMELIN <39401347+malcomhamelin@users.noreply.github.com>
2019-01-12 19:01:47 +0100

    Delete script_managementpost.js

--- JS/modules/mod_managementpost/script_managementpost.js
+++
@@ -1,8 +0,0 @@
-function checkpublication(result) {
-    if (result) {
-        window.alert("Vous avez bien créé une nouvelle catégorie !");
-    }
-    else {
-        window.alert("Impossible de créer cette catégorie : une catégorie du même nom existe déjà");
-    }
-}
