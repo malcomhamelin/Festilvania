e7974bef30431170f77d6131474a75cfcd1fb685
kyuuruu <45665066+kyuuruu@users.noreply.github.com>
2019-01-16 14:12:12 +0100

    redirection

--- Festilvania/modules/mod_register/model_register.php
+++ Festilvania/modules/mod_register/model_register.php
@@ -56,48 +56,48 @@ Class ModelRegister extends Connection {
                             $req->bindParam(':Anniv', $anniversaire);
                             $req -> execute();
 
-                             // echo '<script type="text/javascript">
-                             //        location.href = \'index.php\';
-                             //        window.alert("profil crée");
-                             //    </script>';
+                             echo '<script type="text/javascript">
+                                    location.href = \'index.php\';
+                                    window.alert("profil crée");
+                                </script>';
                         }
                         else {
                             
-                            // echo '<script type="text/javascript">
-                            //         location.href = \'index.php?mod=register\';
-                            //         window.alert("pseudo pris");
-                            //     </script>';
+                            echo '<script type="text/javascript">
+                                    location.href = \'index.php?mod=register\';
+                                    window.alert("pseudo pris");
+                                </script>';
                         }
                     }
                     else {
                         
-                         // echo '<script type="text/javascript">
-                         //            location.href = \'index.php?mod=register\';
-                         //            window.alert("email pris");
-                         //        </script>';
+                         echo '<script type="text/javascript">
+                                    location.href = \'index.php?mod=register\';
+                                    window.alert("email pris");
+                                </script>';
                     }
                 }
                 else{
                     
-                     // echo '<script type="text/javascript">
-                     //                location.href = \'index.php?mod=register\';
-                     //                window.alert("mot de passe différent");
-                     //            </script>';
+                     echo '<script type="text/javascript">
+                                    location.href = \'index.php?mod=register\';
+                                    window.alert("mot de passe différent");
+                                </script>';
                 }
             }
             else{
                 
-                 // echo '<script type="text/javascript">
-                 //                    location.href = \'index.php?mod=register\';
-                 //                    window.alert("inscription impossible,caractères spéciaux interdit");
-                 //                </script>';
+                 echo '<script type="text/javascript">
+                                    location.href = \'index.php?mod=register\';
+                                    window.alert("inscription impossible,caractères spéciaux interdit");
+                                </script>';
             }
         }
         else{
-            // echo '<script type="text/javascript">
-            //                         location.href = \'index.php?mod=register\';
-            //                         window.alert("donnée manquante");
-            //                     </script>';
+            echo '<script type="text/javascript">
+                                    location.href = \'index.php?mod=register\';
+                                    window.alert("donnée manquante");
+                                </script>';
         }
     }
     
